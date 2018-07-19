3ba2783699936682c0f888de6878f7cbb8fb7828
Draw_Piety <39393702+DRAWPIETY@users.noreply.github.com>
2018-07-19 19:42:15 +0800

    Add files via upload

---
+++ hanzi.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+dict={
+    '姓名':'池庆',
+    '年龄':20,
+    '学号':'1627405110'
+}
+line = None
+for key in dict:
+    if line != None:
+        line += ','
+    else:
+        line = '{'
+    line += "'" + key +"':" + str(dict[key])
+line += '}'
+print line              #字典中是无序的
