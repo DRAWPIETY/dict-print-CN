f4754eaca7f1dd16fa49195889e0202e64a6813c
Draw_Piety <39393702+DRAWPIETY@users.noreply.github.com>
2018-07-26 21:49:31 +0800

    Update hanzi.py

--- hanzi.py
+++ hanzi.py
@@ -1,5 +1,5 @@
 #coding: utf-8
-
+#能输出完整的字典格式
 dict={
     '姓名':'大哥大',
     '年龄':20,
@@ -14,3 +14,16 @@ for key in dict:
     line += "'" + key +"':" + str(dict[key])
 line += '}'
 print line              #字典中是无序的
+
+
+'''
+#确保字典中中文的输出
+#coding: utf-8
+dict={
+    '姓名':'大哥大',
+    '年龄':20,
+    '学号':'1627405110'
+}
+for key in dict:
+    print key+':'+dict[key]
+'''
